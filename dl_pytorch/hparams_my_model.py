42fa9fd4f950439bfe8e418714e04a07c513282a
fadibenz <f.benzaima@esi-sba.dz>
2025-02-06 13:16:17 +0000

    Added my own architecture with empirical hyper-params fine-tuning

--- dl_pytorch/hparams_my_model.py
+++ dl_pytorch/hparams_my_model.py
@@ -1,13 +1,13 @@
 from argparse import Namespace
 
 HP = Namespace(
-    batch_size=32,
-    lr=1e-3,
+    batch_size=128,
+    lr=2e-3,
     momentum=0.9,
-    lr_decay=0.99,
+    lr_decay=0.95,
     optim_type="adam",
     l2_reg=0.0,
-    epochs=5,
+    epochs=10,
     do_batchnorm=True,
     p_dropout=0.2
 )
--- dl_pytorch/my_model.py
+++ dl_pytorch/my_model.py
@@ -2,106 +2,63 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-#######################################################################
-# TODO: Design your own neural network
-# You can define utility functions/classes here
-#######################################################################
-
-#######################################################################
-# End of your code
-#######################################################################
-
 
 class MyNeuralNetwork(nn.Module):
-    def __init__(self, do_batchnorm=False, p_dropout=0.0):
+  def __init__(self, do_batchnorm=False, p_dropout=0.0):
         super().__init__()
         self.do_batchnorm = do_batchnorm
         self.p_dropout = p_dropout
 
-
-        self.conv11 = nn.Conv2d(3, 32, 3, padding= 1)
+        self.conv1 = nn.Conv2d(3, 32, 3, padding= 1)
         if self.do_batchnorm:
-          self.bn11 = nn.BatchNorm2d(32)
-        self.gelu11 = nn.ReLU()
-        
-        self.conv12 = nn.Conv2d(32, 32, 3, padding= 1)
-        if self.do_batchnorm:
-          self.bn12 = nn.BatchNorm2d(32)
-        self.gelu12 = nn.ReLU()
+          self.bn1 = nn.BatchNorm2d(32)
+        self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(2, 2) 
 
-
-        self.conv21 = nn.Conv2d(32, 64, 3, padding= 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding= 1)
         if self.do_batchnorm:
-          self.bn21 = nn.BatchNorm2d(64)
-        self.gelu21 = nn.ReLU()
+          self.bn2 = nn.BatchNorm2d(64)
+        self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(2, 2) 
-        
-        self.conv31 = nn.Conv2d(64, 128, 3, padding= 1)
-        if self.do_batchnorm:
-          self.bn31 = nn.BatchNorm2d(128)
-        self.gelu31 = nn.ReLU()
 
-        self.conv32 = nn.Conv2d(128, 128, 3, padding= 1)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding= 1)
         if self.do_batchnorm:
-          self.bn32 = nn.BatchNorm2d(128)
-        self.gelu32 = nn.ReLU()
+          self.bn3 = nn.BatchNorm2d(128)
+        self.relu3 = nn.ReLU()
         self.pool3 = nn.MaxPool2d(2, 2) 
 
-        self.fc1 = nn.Linear(2048, 256)
+        self.fc1 = nn.Linear(2048, 356)
+        if self.do_batchnorm:
+          self.bn4 = nn.BatchNorm1d(356)
         self.relu4 = nn.ReLU()
         if p_dropout > 0.0:
           self.drop = nn.Dropout(p=p_dropout)
-        
-        self.fc2 = nn.Linear(256,100)
-        self.relu5 = nn.ReLU()
+        self.fc2 = nn.Linear(356, 100)
 
-        self.fc3 = nn.Linear(100 , 100)
-        
-    def forward(self, x):
+  def forward(self, x):
 
-        x = self.conv11(x)  
+        x = self.conv1(x)  
         if self.do_batchnorm:
-            x = self.bn11(x)
-        x = self.gelu11(x)
-        
-        x = self.conv12(x)  
-        if self.do_batchnorm:
-            x = self.bn12(x)
-        x = self.gelu12(x)
-        
-        x = self.pool1(x)
+            x = self.bn1(x)
+        x = self.pool1(self.relu1(x))  
 
-        x = self.conv21(x)  
-        
+        x = self.conv2(x)  
         if self.do_batchnorm:
-            x = self.bn21(x)
-        x = self.gelu21(x)
-                
-        x = self.pool2(x)
-        
-        x = self.conv31(x)  
-        
+            x = self.bn2(x)
+        x = self.pool2(self.relu2(x))  
+        x = self.conv3(x)  
         if self.do_batchnorm:
-            x = self.bn31(x)
-        x = self.gelu31(x)
-        
-        x = self.conv32(x)  
-        if self.do_batchnorm:
-            x = self.bn32(x)
-        x = self.gelu32(x)
-        
+            x = self.bn3(x)
+        x = self.relu3(x)
         x = self.pool3(x)
 
-        x = torch.flatten(x, 1) 
-        x = self.relu4(self.fc1(x))  
+        x = torch.flatten(x, 1)  
+        x = self.fc1(x)
+        if self.do_batchnorm:
+          x = self.bn4(x)
         
+        x = self.relu4(x) 
         if self.p_dropout > 0.0:
             x = self.drop(x)
-        
-        x = self.relu5(self.fc2(x))  
-        if self.p_dropout > 0.0:
-            x = self.drop(x)
-        x = self.fc3(x)  
-            
+        x = self.fc2(x)  
         return x
